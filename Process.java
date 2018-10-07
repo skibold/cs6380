2cad7855decbc4f66b9bc25cba946139749968e6
john <johnscebold@sbcglobal.net>
2018-10-07 16:56:52 -0500

    algorithm works correctly

--- Process.java
+++ Process.java
@@ -52,12 +52,12 @@ public class Process implements Runnable { //extends Thread {
 
 	public void join() throws InterruptedException {
 		if(t != null) t.join();
+		Logger.normal(instancename, "finish", "round " + (round-1));
 	}
 
 	//override
 	public void run() {
 		final String method = "run";
-		if(term) return;
 		if(init) {
 			Logger.normal(instancename, method, "First round, send explore on all edges: " + logEdges);
 			init = false;
@@ -79,15 +79,9 @@ public class Process implements Runnable { //extends Thread {
 				Logger.normal(instancename, method, "No message from " + sender);
 				continue;
 			}
-			Logger.normal(instancename, method, "Received " + m + " from " + sender);
-			// was I waiting for a response from this other process?
-			// if so, now I have it, so delete the waiting flag
-			if(awaitingResponse.containsKey(sender)) {
-				Logger.normal(instancename, method, "Delete awaitingResponse(" + sender + ", " + awaitingResponse.get(sender) + ")");
-				awaitingResponse.remove(sender);
-			}
+			Logger.normal(instancename, method, "Received " + m + " from " + sender);			
 
-			if(leader2forward != null) continue; // do nothing, but drain queues on all other edges
+			if(term || leader2forward != null) continue; // do nothing, but drain queues on all other edges
 			
 			if(m.isLeader()) {
 				leader2forward = m;
@@ -97,7 +91,7 @@ public class Process implements Runnable { //extends Thread {
 			else if(m.isExplore()) {
 				// add m to list of messages i have received which i must respond to
 				pendingResponse.put(sender, m);
-				Logger.normal(instancename, method, "I must respond to " + sender);
+				Logger.debug(instancename, method, "I must respond to " + sender);
 				// remember the largest
 				if(m.originator() > largestId) {
 					largestId = m.originator();
@@ -105,13 +99,25 @@ public class Process implements Runnable { //extends Thread {
 					bestExplore = m;
 					explore2forward = m;
 				}
+				if(m.originator() >= largestId) {
+					// I might've been waiting for a response from this sender, 
+					// but he just sent me an even better leader candidate, 	
+					// so delete the awaitingResponse flag if there was one.  
+					// I might get a nack later from this sender, but it won't matter.
+					responseFrom(sender);
+				}
 			}
-			else if(m.isAck()) {
-				// if this is an ack, then make the sender my child
-				links2children.add(e.otherSide(uid));
-				Logger.normal(instancename, method, sender + " sent me an ACK, he is my newest child.");
+			else {
+				if(m.isAck() && !links2children.contains(e.otherSide(uid))) {
+					// if this is an ack, then make the sender my child
+					links2children.add(e.otherSide(uid));
+					Logger.debug(instancename, method, sender + " sent me an ACK, he is my newest child.");
+				}
+				responseFrom(sender);
 			}
 		}
+
+		if(term) { Logger.normal(instancename, method, "already terminated"); return; }
 
 		// send out all messages
 		// forward the leader message if it exists and terminate
@@ -120,8 +126,8 @@ public class Process implements Runnable { //extends Thread {
 			for(Integer i : links2children) {
 				Edge e = edgeMap.get(i);
 				Message leaderClone = Message.cloneForNewRecipient(leader2forward, e.otherSide(uid));
-				e.send(uid, leaderClone);
 				Logger.normal(instancename, method, "Forward " + leaderClone);
+				e.send(uid, leaderClone);
 			}
 			term = true;
 			return;
@@ -129,12 +135,16 @@ public class Process implements Runnable { //extends Thread {
 
 		// forward the explore message if I have one
 		if(explore2forward != null) {
-			Logger.normal(instancename, method, largestId + " is the largest I've seen. Forward to everyone. Set new parent = " + link2parent.otherSide(uid));
+			Logger.normal(instancename, method, 
+				largestId + " is the largest I've seen. Forward to everyone. Set new parent = " + link2parent.otherSide(uid));
 			for(Integer recipient : edgeMap.keySet()) {
-				Edge e = edgeMap.get(recipient);
-				if(e != link2parent) { // don't forward back to sender
+				Message m = pendingResponse.get(recipient); // might be a message I receive this round, but what I have to forward is better
+				if(m == null || m.originator() != explore2forward.originator()) {
+					Edge e = edgeMap.get(recipient);
 					Message exploreClone = Message.cloneForNewRecipient(explore2forward, recipient);
-					Logger.normal(instancename, method, "Forward " + exploreClone + ". This counts as a response TO (" + recipient + "," + pendingResponse.get(recipient) + "). Now I need a response FROM " + recipient);
+					Logger.normal(instancename, method, "Forward " + exploreClone);
+					Logger.debug(instancename, method, "This counts as a response TO (" + recipient + "," + 
+						pendingResponse.get(recipient) + "). Now I need a response FROM " + recipient);
 					awaitingResponse.put(recipient, exploreClone);
 					pendingResponse.remove(recipient);
 					e.send(uid, exploreClone);
@@ -150,9 +160,9 @@ public class Process implements Runnable { //extends Thread {
 			if(m != bestExplore) {
 				// this message was useless to me, send nack, delete
 				Message nack = Message.nack(m);
+				Logger.normal(instancename, method, "Send " + nack + " to " + sender);
 				edgeMap.get(sender).send(uid, nack);
 				toDelete.add(sender);
-				Logger.normal(instancename, method, "Send " + nack + " to " + sender + " in response to " + m);
 			}
 		}
 		for(Integer sender : toDelete) {
@@ -162,17 +172,17 @@ public class Process implements Runnable { //extends Thread {
 		// send back acks if I'm not waiting for responses from anyone
 		if(awaitingResponse.isEmpty()) {
 			Logger.normal(instancename, method, "I have all my responses.");
-			if(link2parent != null) {
+			if(link2parent != null && pendingResponse.get(link2parent.otherSide(uid)) != null) {
 				Message ack = Message.ack(bestExplore);
-				link2parent.send(uid, ack);
-				Logger.normal(instancename, method, "Send " + ack + " to parent(" + link2parent.otherSide(uid) + ") in response to " + pendingResponse.get(link2parent.otherSide(uid)));
+				Logger.normal(instancename, method, "Send " + ack + " to parent(" + link2parent.otherSide(uid) + ")");
+				link2parent.send(uid, ack);	
 				pendingResponse.remove(link2parent.otherSide(uid));
 			} else if(largestId == uid) {
 				for(Integer i : links2children) {
 					Edge e = edgeMap.get(i);
 					Message imTheLeader = Message.lead(uid, e.otherSide(uid));
-					e.send(uid, imTheLeader);
 					Logger.normal(instancename, method, "I am the leader. Send " + imTheLeader);
+					e.send(uid, imTheLeader);
 				}
 				term = true;
 			}
@@ -201,5 +211,28 @@ public class Process implements Runnable { //extends Thread {
 		this.link2parent = null;
 		this.init = true;
 		this.term = false;
+	}
+
+	public String children() {
+		String children = new String();
+		if(links2children.isEmpty()) return children;
+		for(Integer i : links2children) {
+			children += i.toString() + ", ";
+		}
+		return children.substring(0,children.length()-2);
+	}
+
+	public String parent() {
+		if(link2parent == null) return "None";
+		return Integer.toString(link2parent.otherSide(uid));
+	}
+
+	private void responseFrom(int sender) {
+		// was I waiting for a response from this other process?
+		// if so, now I have it, so delete the waiting flag
+		if(awaitingResponse.containsKey(sender)) {
+			Logger.debug(instancename, "responseFrom", "Delete awaitingResponse(" + sender + ", " + awaitingResponse.get(sender) + ")");
+			awaitingResponse.remove(sender);
+		}
 	}
 }
