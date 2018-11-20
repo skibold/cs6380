ca8db3d144894ee662e5ac6fcd6266a5761cd151
john <johnscebold@sbcglobal.net>
2018-11-20 13:01:51 -0600

    Added logic for the test, accept, and reject messages

--- cs6380_2/Process.java
+++ cs6380_2/Process.java
@@ -22,13 +22,16 @@ public class Process implements Runnable { //extends Thread {
 	private ArrayList<Edge> outsideEdges; // edges which might be outside my component, initially all incident edges
 	private ArrayList<Edge> rejectedEdges; // edges which lead to processes in my component, but are not part of the MST
 	
-	
+	private Integer awaitingResponseTest; // I am waiting for accept/reject from process with this uid
+	private ArrayList<Message> pendingResponseTest; // I must respond to the senders with accept/reject	
+
 	public Process(int uid, ArrayList<Edge> edges) {
 		this.uid = uid;
 		this.cid = Integer.toString(uid);
 		this.level = 0;
 		this.round = 0;
 		instancename = classname + "(" + uid + ")";
+		this.pendingResponseTest = new ArrayList<Message>();
 		this.outsideEdges = new ArrayList<Edge>();
 		this.componentEdges = new ArrayList<Edge>();
 		this.rejectedEdges = new ArrayList<Edge>();
@@ -36,11 +39,11 @@ public class Process implements Runnable { //extends Thread {
 		this.logEdges = "{";
 		for(Edge e : edges) {
 			logEdges += e.toString() + ", ";
-			// add the edge to outsideEdges and keep it sorted
+			insertionSort(outsideEdges, e); // keep them sorted least to greatest by weight
 			edgeMap.put(e.otherSide(uid), e);
 		}
 		this.logEdges += "}";
-		
+		this.awaitingResponseTest = null;
 		this.link2parent = null;
 		this.mwoe = null;
 		this.begin = true;
@@ -144,7 +147,24 @@ public class Process implements Runnable { //extends Thread {
 		final String method = "sendTestMsg";
 		Logger.entering(instancename, method);
 
-		
+		if(outsideEdges.isEmpty()) {
+			this.mwoe = null;
+			Logger.normal(instancename, method, "No more edges to test, no mwoe");
+			Logger.exiting(instancename, method);
+			return false;
+		}
+		if(this.awaitingResponseTest != null) {
+			Logger.normal(instancename, method, "Already have a test message out to " + 
+								awaitingResponseTest + ", do nothing");
+			Logger.exiting(instancename, method);
+			return false;
+		}
+
+		this.mwoe = outsideEdges.get(0); // outside edges already sorted, pending mwoe assignment
+		this.awaitingResponseTest = this.mwoe.otherSide(this.uid);
+		Logger.normal(instancename, method, " to " + this.awaitingResponseTest);
+		Message test = Message.test(this.uid, this.awaitingResponseTest, this.level, this.cid);
+		this.mwoe.send(this.uid, test);
 		
 		Logger.exiting(instancename, method);
 		return true;
@@ -153,7 +173,21 @@ public class Process implements Runnable { //extends Thread {
 		final String method = "receiveTestMsg";
 		Logger.entering(instancename, method);
 
-		
+		Logger.normal(instancename, method, "this.cid = " + this.cid + ", m.cid = " + m.cid() + 
+						", this.level = " + this.level + ", m.level = " + m.level());
+		pendingResponseTest.remove(m); // in case this is a re-process of the message
+		Edge back = edgeMap.get(m.originator());
+		if(this.cid == m.cid()) {
+			Logger.debug(instancename, method, "reply reject");
+			back.send(this.uid, Message.reject(m));
+		} else if(this.level >= m.level()) {
+			Logger.debug(instancename, method, "reply accept");
+			back.send(this.uid, Message.accept(m));
+		} else {
+			// defer the response
+			Logger.debug(instancename, method, "defer response");
+			this.pendingResponseTest.add(m);
+		}
 
 		Logger.exiting(instancename, method);
 	}
@@ -162,7 +196,14 @@ public class Process implements Runnable { //extends Thread {
 	private void receiveAcceptMsg(Message m) {
 		final String method = "receiveAcceptMsg";
 		Logger.entering(instancename, method);
-		
+		Logger.debug(instancename, method, m.originator() + " " + awaitingResponseTest);
+
+		if(awaitingResponseTest != null && m.originator() == awaitingResponseTest) {
+			awaitingResponseTest = null;
+			sendReportMsg();
+		} else {
+			Logger.warning(instancename, method, "Didn't expect this accept, do nothing");
+		}
 
 		Logger.exiting(instancename, method);
 	}
@@ -171,7 +212,21 @@ public class Process implements Runnable { //extends Thread {
 	private void receiveRejectMsg(Message m) {
 		final String method = "receiveRejectMsg";
 		Logger.entering(instancename, method);
+		Logger.debug(instancename, method, m.originator() + " " + awaitingResponseTest);
+
+		if(awaitingResponseTest != null && m.originator() == awaitingResponseTest) {
+			Edge reject = edgeMap.get(m.originator());
+			Logger.normal(instancename, method, "remove outsideEgde" + reject.toString() + 
+								" and insert into rejectedEdges");
+			insertionSort(rejectedEdges, reject);
+			outsideEdges.remove(reject);
 		
+			this.awaitingResponseTest = null;
+			if(!sendTestMsg())
+				sendReportMsg();
+		} else {
+			Logger.warning(instancename, method, "Didn't expect this reject, do nothing");
+		}
 
 		Logger.exiting(instancename, method);		
 	}
@@ -204,6 +259,30 @@ public class Process implements Runnable { //extends Thread {
 	}
 
 /*** end functions to handle messages ***/
+
+	private void insertionSort(ArrayList<Edge> collection, Edge e) {
+		final String method = "insertionSort";
+		if(e == null) {
+			Logger.debug(instancename, method, "Not gonna insert null");
+			return;
+		}
+
+		int i = collection.size();
+		if(!collection.isEmpty()) {
+			for(i=0; i<collection.size(); i++) {
+				if(collection.get(i).compare(e) == 1) { // e is smaller than element at i
+					break;
+				}
+			}
+		}
+		if(i < collection.size()) {
+			Logger.debug(instancename, method, "Insert " + e.toString() + " at index " + i);
+			collection.add(i,e);
+		} else {
+			Logger.debug(instancename, method, "Insert " + e.toString() + " at end");
+			collection.add(e);
+		}
+	}
 
 
 	//override
